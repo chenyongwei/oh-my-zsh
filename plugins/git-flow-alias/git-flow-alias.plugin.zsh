c685a7cdfc72376732241dfd1de99630066ae75c
Lanvige Jiang <lanvige@gmail.com>
2012-08-14 17:21:34 +0800

    add git flow alias

---
+++ plugins/git-flow-alias/git-flow-alias.plugin.zsh
@@ -0,0 +1,25 @@
+# The git-flow don't define the alias
+
+alias gfi='git flow init -d'
+
+# alias for feature
+alias gffs='git flow feature start'
+alias gffc='git flow feature checkout'
+alias gffp='git flow feature publish'
+alias gfft='git flow feature track'
+alias gffl='git flow feature pull'
+alias gffr='git flow feature rebase'
+alias gffd='git flow feature diff'
+alias gfff='git flow feature finish'
+
+# alias for release
+alias gfrs='git flow release start'
+alias gfrp='git flow release public'
+alias gfrt='git flow release track'
+alias gfrf='git flow release finish'
+
+# alias for hotfix
+alias gfhs='flow hotfix start'
+alias gfhp='flow hotfix publish'
+alias gfht='flow hotfix track'
+alias gfhf='flow hotfix finish'
